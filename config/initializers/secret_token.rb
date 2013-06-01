00de3b5fd09eb50db6227228934aa98b7b00e6f3
O. Thebe <othebe@gmail.com>
2013-06-01 13:50:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Queueless::Application.config.secret_token = '8f80f5e3c3cf9d2deb0d93deebbfc12bd30c9edae09ec6c9f7555670bd482fd6266a14d18ddd50f0be8ee327fe630b8294f6295160651d2b076a28834d9ba743'
